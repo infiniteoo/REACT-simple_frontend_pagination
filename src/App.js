1c8a4255a9453e29b49bd65b7d8c717e3918f782
Troy Dorman <troydorman@gmail.com>
2021-07-22 19:56:09 -0600

    did more cleanup.  created our states for the project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { useState, useEffect } from "react";
+
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
   return (
     <div className="container">
       <h1>My App</h1>
