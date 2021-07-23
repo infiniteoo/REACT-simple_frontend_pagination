d42e3c4d479f8fc7ab7619101a51bda4870fb722
Troy Dorman <troydorman@gmail.com>
2021-07-22 19:57:56 -0600

    created useeffect to fetch dummy posts from our API

--- src/App.js
+++ src/App.js
@@ -9,6 +9,17 @@ function App() {
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setPostsPerPage] = useState(10);
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className="container">
       <h1>My App</h1>
